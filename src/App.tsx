ec1125c96f04db7d2319ccbfc9796e4ca2c55160
kritical0613 <kulishan0613@gmail.com>
2022-03-09 23:19:50 -0600

    setup environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
+import Home from './pages/Home';
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className='bg-cover bg-mobileBackground laptop:bg-background'>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </div>
+        </div>
+      </Suspense>
+    </Router>
   );
 }
 
-export default App;
+export default React.memo(App)
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React, { useMemo } from 'react'
+const Home: React.FC = () => {
+  return (
+    <div className='pt-[97px] m-auto'>
+      AAA
+    </div>
+  );
+}
+
+export default Home;
